0459e60fcb8f47e85327b9b55fa41afb903a3aab
Patryk Peczak <ppeczak0405@gmail.com>
2021-06-21 23:16:23 +0200

    Add names to GUI elements, events for butttons and small part of game backend

---
+++ Game.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory
+{
+    public static class MyExtensions
+    {
+        private static Random rng = new Random();
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+
+    public class Game
+    {
+        private const int memDim = 4;
+        private int[,] gameArray;
+        private int pressedFields;
+        MainWindow mWindow;
+
+        public Game(MainWindow w)
+        {
+            mWindow = w;
+            pressedFields = 0;
+            gameArray = new int[memDim, memDim];
+            fillGameArray();
+        }
+        private void succesMatch(Tuple<int, int> f, Tuple<int, int> s)
+        {
+
+        }
+        private List<int> getRandom()
+        {
+            var result = new List<int>(memDim * memDim);
+            for(int i = 0; i < memDim * memDim; i++){
+                result.Add(i % (memDim * memDim) / 2);
+            }
+            result.Shuffle();
+            return result;
+        }
+        private void fillGameArray()
+        {
+            List<int> randomList = getRandom();
+            for(int i = 0; i < memDim; i++){
+                for(int j = 0; j < memDim; j++){
+                    gameArray[i,j] = randomList[i * memDim + j];
+                }
+            }
+        }
+        public void displayTable()
+        {
+            for(int i = 0; i < memDim; i++){
+                for(int j = 0; j < memDim; j++){
+                    System.Console.Write(gameArray[i, j].ToString() + " ");
+                }
+                System.Console.WriteLine();
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,99 @@ namespace Memory
     /// </summary>
     public partial class MainWindow : Window
     {
+        Game game;
         public MainWindow()
         {
             InitializeComponent();
+            game = new Game(this);
+        }
+        private void exitClick(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+        private void playClick(object sender, RoutedEventArgs e)
+        {
+            game.displayTable();
+        }
+        private void mem00Click(object sender, RoutedEventArgs e)
+        {
+            Button s = (Button)sender;
+            System.Console.WriteLine(s.Name);
+        }
+        private void mem01Click(object sender, RoutedEventArgs e)
+        {
+            Button s = (Button)sender;
+            System.Console.WriteLine(s.Name);
+        }
+        private void mem02Click(object sender, RoutedEventArgs e)
+        {
+            Button s = (Button)sender;
+            System.Console.WriteLine(s.Name);
+        }
+        private void mem03Click(object sender, RoutedEventArgs e)
+        {
+            Button s = (Button)sender;
+            System.Console.WriteLine(s.Name);
+        }
+        private void mem10Click(object sender, RoutedEventArgs e)
+        {
+            Button s = (Button)sender;
+            System.Console.WriteLine(s.Name);
+        }
+        private void mem11Click(object sender, RoutedEventArgs e)
+        {
+            Button s = (Button)sender;
+            System.Console.WriteLine(s.Name);
+        }
+        private void mem12Click(object sender, RoutedEventArgs e)
+        {
+            Button s = (Button)sender;
+            System.Console.WriteLine(s.Name);
+        }
+        private void mem13Click(object sender, RoutedEventArgs e)
+        {
+            Button s = (Button)sender;
+            System.Console.WriteLine(s.Name);
+        }
+        private void mem20Click(object sender, RoutedEventArgs e)
+        {
+            Button s = (Button)sender;
+            System.Console.WriteLine(s.Name);
+        }
+        private void mem21Click(object sender, RoutedEventArgs e)
+        {
+            Button s = (Button)sender;
+            System.Console.WriteLine(s.Name);
+        }
+        private void mem22Click(object sender, RoutedEventArgs e)
+        {
+            Button s = (Button)sender;
+            System.Console.WriteLine(s.Name);
+        }
+        private void mem23Click(object sender, RoutedEventArgs e)
+        {
+            Button s = (Button)sender;
+            System.Console.WriteLine(s.Name);
+        }
+        private void mem30Click(object sender, RoutedEventArgs e)
+        {
+            Button s = (Button)sender;
+            System.Console.WriteLine(s.Name);
+        }
+        private void mem31Click(object sender, RoutedEventArgs e)
+        {
+            Button s = (Button)sender;
+            System.Console.WriteLine(s.Name);
+        }
+        private void mem32Click(object sender, RoutedEventArgs e)
+        {
+            Button s = (Button)sender;
+            System.Console.WriteLine(s.Name);
+        }
+        private void mem33Click(object sender, RoutedEventArgs e)
+        {
+            Button s = (Button)sender;
+            System.Console.WriteLine(s.Name);
         }
     }
 }
